61b8d17d882026d550fcdfb93a3871289d82b5cb
AJ Tran <31839316+ajtran303@users.noreply.github.com>
2020-11-25 20:38:16 -0700

    Add Helper module to spec/support

---
+++ spec/support/helpers.rb
@@ -0,0 +1,13 @@
+module Helpers
+  def page_does thing
+    expect(page).to thing
+  end
+
+  def page_has_link link
+    page_does have_link *link
+  end
+
+  def page_has_text text
+    page_does have_text text
+  end
+end
